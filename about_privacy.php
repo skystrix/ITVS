17ffcac337094dfaf4f32944a3cfdeaf6e7ad3d9
Santanu Das <santanud.411@gmail.com>
2017-03-20 20:39:35 +0530

    Added initial files

---
+++ about_privacy.php
@@ -0,0 +1,34 @@
+ <?php include("header1.php") ?>	
+<div class="about_banner">
+	<div class="container">
+		<h2>About Privacy Policy</h2>
+	    <span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <a href="about.html" class="home_desc">About</a> / <span>About_Privacy</span></span>
+	</div>
+</div>
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 column-10">
+	</div>
+	<div class="col-md-9 column-9">
+        <h3>PrivacyPolicy.com,</h3>
+        <p class="m_3">
+            Personal Information<br>
+D-Fut Technology and Marketing Pvt Ltd is the licensed owner of the brand ITVS and the website itvs.co.in. D-Fut respects your privacy. This Privacy Policy provides succinctly the manner your data is collected and used by D-fut on the Site. As a visitor to the Site/ Customer you are advised to please read the Privacy Policy carefully. By accessing the services provided by the Site you agree to the collection and use of your data by D-Fut in the manner provided in this Privacy Policy.
+<br><br>
+Services overview<br>
+As part of the registration process on the Site, D-Fut may collect the following personally identifiable information about you: Name including first and last name, alternate email address, mobile phone number and contact details, Postal code, Demographic profile (like your age, gender, occupation, education, address etc.) and information about the pages on the site you visit/access, the links you click on the site, the number of times you access the page and any such browsing information
+
+<br><br>
+Eligibility<br>
+Services of the Site would be available to only select geographies in India. Persons who are "incompetent to contract" within the meaning of the Indian Contract Act, 1872 including un-discharged insolvents etc. are not eligible to use the Site. If you are a minor i.e. under the age of 18 years but at least 13 years of age you may use the Site only under the supervision of a parent or legal guardian who agrees to be bound by these Terms of Use. If your age is below 18 years your parents or legal guardians can transact on behalf of you if they are registered users.
+
+<br><br>
+Cancellation by Site / Customer<br>
+You as a customer can cancel your order within 24 hours. In such a case we will refund any payments already made by you for the order. If we suspect any fraudulent transaction by any customer or any transaction which defies the terms & conditions of using the website, we at our sole discretion could cancel such orders. We will maintain a negative list of all fraudulent transactions and customers and would deny access to them or cancel any orders placed by them.
+        </p>
+     </div>
+   </div>
+</div>
+
+
+       <?php include("footer.php") ?>	
