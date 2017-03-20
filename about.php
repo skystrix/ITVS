e7ec80eca58c234a4d009928e4c267f79eda0c89
Santanu Das <santanud.411@gmail.com>
2017-03-20 20:38:32 +0530

    Added initial files

---
+++ about.php
@@ -0,0 +1,176 @@
+ 
+<?php include("header1.php") ?>	
+<div class="about_banner">
+	<div class="container">
+		<h2>About Fullwidth</h2>
+		<span class="breadcrumbs"><a href="index.html"><i class="fa fa-home home_1"></i></a> / <span>About</span></span>
+	</div>
+</div>
+<div class="about_top">
+ <div class="container">
+	<div class="col-md-3 column-1">
+	  <div class="list-group">
+        <a href="#" class="list-group-item active">About Community</a>
+        <a href="about_work.html" class="list-group-item ">How It Works</a>
+        <a href="about_privacy.php" class="list-group-item ">Privacy Policy</a>
+        <a href="about_terms.html" class="list-group-item ">Terms Of Use</a>
+        <a href="about_tools.html" class="list-group-item ">Tools</a>
+        <a href="contact.html" class="list-group-item ">Contact Us</a>
+       </div>
+	</div>
+	<div class="col-md-9 column-2">
+		<div id="column" class="sub_col" style="height: 65px;">
+			<div class="text-inner"><p>&nbsp;&nbsp;&nbsp;&nbsp;01</p>
+			  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ABOUT US</p>
+			</div>
+	    </div>	
+	    <div id="sub_col2">
+			<h3>About ITVS Community</h3>
+			<h4>We started our services on 2015</h4>
+		 </div>	
+	     <div class="clearfix"> </div>
+	     <p class="m_1">ITVS is your key to build the power of the internet. This unique and revolutionary model allows you to Learn and Earn. Since time immemorial, mankind has acquired knowledge and has been able to put it to constructive use, to earn one's livelihood.</p>
+	     <p class="m_1 m_2">ITVS presents an opportunity that will not only provide you knowledge and education but will bring you face to face with the vast network of information and opportunity, the Internet, besides presenting you an opportunity to develop / grow and achieve financial growth.</p>
+	</div>		
+    <div class="clearfix"> </div>
+ </div>
+</div>
+<div class="about_middle">
+ <div class="container">
+ 	 <div id="column" class="sub_col sub_col1" style="height: 65px;">
+		<div class="text-inner"><p>&nbsp;&nbsp;&nbsp;&nbsp;02</p>
+		  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Testimonials</p>
+		</div>
+	 </div>
+	<div class="column_5">
+ 	  <div class="col-md-4">
+	  <div class="item-testimonial">
+        <div class="content_box">
+            <blockquote>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page.  
+                            </p>    
+                <footer>reader - <a href="#">distracted</a></footer>
+            </blockquote>
+          </div>
+           <div class="avatar"><img src="images/pic5.jpg" class="img-responsive" alt=""/></div>
+        </div>
+      </div>
+      <div class="col-md-4">
+	  <div class="item-testimonial">
+        <div class="avatar"><img src="images/pic12.jpg" class="img-responsive" alt=""/></div>
+          <div class="content_box">
+            <blockquote>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have in some form.  
+                            </p>    
+                <footer>suffered - <a href="#">alteration</a></footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+	  <div class="item-testimonial item-testimonial_last">
+        <div class="avatar"><img src="images/pic6.jpg" class="img-responsive" alt=""/></div>
+          <div class="content_box">
+            <blockquote>
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of  Latin.  
+                            </p>    
+                <footer>literature - <a href="#">classical</a></footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <div class="clearfix"> </div>
+   </div>
+ </div>
+</div>
+<div class="about_bottom">
+  <div class="container">
+  	<div id="column" class="sub_col sub_col1" style="height: 65px;">
+		<div class="text-inner"><p>&nbsp;&nbsp;&nbsp;&nbsp;03</p>
+		  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Who we are</p>
+		</div>
+	 </div>
+	<div class="col-md-3 column-6">
+	</div>
+	<div class="col-md-9 column-7">
+		<h3>Lorem Ipsum</h3>
+		<h4>Ipsum generators</h4>
+		<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+		<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum".</p>
+		<div class="footer-social about_social">
+		   <a target="_blank" href="" hidefocus="true" style="outline: none;"><i class="fa fa-facebook"></i></a>
+			<a target="_blank" href="" hidefocus="true" style="outline: none;"><i class="fa fa-twitter"></i></a>
+			<a target="_blank" href="" hidefocus="true" style="outline: none;"><i class="fa fa-google-plus"></i></a>
+	    </div>
+	    <div class="but_4">
+	      <a class="readmore" href="#" title="Read More">Read More</a>
+	    </div>
+	</div>		
+    <div class="clearfix"> </div>
+ </div>
+</div>
+<div class="footer_widget">
+	<div class="container">
+		<div class="col-md-4">
+			<aside id="text-1" class="widget widget_text">
+				<h3 class="widget-title"><span>COME VISIT US</span></h3>
+				<div class="textwidget"><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+				   <p><em style="color: #fff;">standard chunk.,<br> Finibus Bonorum,<br> Ipsum generators, treatise</em></p>
+				</div> 
+			</aside>
+		</div>
+		<div class="col-md-4">
+			<aside id="text-2" class="widget widget_text">
+				<h3 class="widget-title"><span>MORE ABOUT US</span></h3>
+				<div class="textwidget">
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum,.</p></div>
+		    </aside>
+		</div>
+		<div class="col-md-4">
+			<aside>
+				<h3 class="widget-title"><span>FLICKR FEED</span></h3>
+				<ul class="gallery">
+					<li><a href="images/pic5.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic5.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li><a href="images/pic6.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic6.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li><a href="images/pic11.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic11.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li class="last">
+						<a href="images/pic12.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic12.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li>
+						<a href="images/pic6.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic6.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li>
+					   <a href="images/pic5.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic5.jpg" class="img-responsive" alt="name">
+					   </a>
+				    </li>
+					<li>
+						<a href="images/pic11.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic11.jpg" class="img-responsive" alt="name">
+					   </a>
+					</li>
+					<li class="last">
+						<a href="images/pic12.jpg" class="img-responsive" title="Rose" rel="title1">
+						<img src="images/pic12.jpg" class="img-responsive" alt="name">
+					   </a>
+				    </li>
+					<div class="clearfix"> </div>
+				</ul>
+			</aside>
+		</div>
+	</div>
+</div>
+<?php include("footer.php") ?>	
